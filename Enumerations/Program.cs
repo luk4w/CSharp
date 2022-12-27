dc8e802654ed23f57367c3d07609602eea7d3fc7
Lucas Franco de Mello <luk4w@outlook.com>
2022-12-27 14:09:20 -0300

    instantiated order class

--- Enumerations/Program.cs
+++ Enumerations/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Enumerations.Entities;
+using Enumerations.Entities.Enums;
+
+namespace Enumerations
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Order order = new Order()
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment,
+
+            };
+
+            Console.WriteLine(order);
+
+            Console.ReadLine();
+        }
+    }
+}
