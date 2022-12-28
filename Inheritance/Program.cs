f7427610492ed55ab0f68cc80dbacfedc6e3a15a
Lucas Franco de Mello <luk4w@outlook.com>
2022-12-28 01:16:25 -0300

    upcasting

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Inheritance
+﻿using Inheritance.Entities;
+
+namespace Inheritance
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Account account = new Account(number: 01, holder: "Renata", balance: 100);
+            BusinessAccount bAcc = new BusinessAccount(number: 02, holder: "Fahrenheit", balance: 0, loanLimit: 500);
+
+            // Upcasting
+            Account acc1 = bAcc;
+            Account acc2 = new BusinessAccount(03, "Gauss", 0, 200);
+            Account acc3 = new SavingsAccount(04, "Joana", 0, 0.01);
+            
             Console.ReadKey();
         }
     }
