de95d0ece47b2149f21c8bb4a0f73feed8edd515
Lucas Franco de Mello <luk4w@outlook.com>
2022-12-30 02:29:14 -0300

    created circle class

---
+++ AbstractMethod/Entities/Circle.cs
@@ -0,0 +1,18 @@
+using AbstractMethod.Entities.Enums;
+
+namespace AbstractMethod.Entities
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
