024a9c23852c8c133ae8f233aee137beacba12fb
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-01 17:46:58 -0300

    created domain exception

---
+++ Exercises/Exceptions/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+namespace Exceptions.Entities.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string message)
+            : base(message)
+        {
+        }
+    }
+}
