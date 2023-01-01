c5e1a2f131621bd438fbe1029784433162524975
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-01 15:01:51 -0300

    created exceptions project

---
+++ Exceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(n1/n2);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine($"Division by zero is not allowed!");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine($"Enter a number! {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
