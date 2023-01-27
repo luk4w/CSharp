80d676231777238a1a422314a7b242d16c5b507c
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-27 20:03:54 -0300

    added icomparable sample

---
+++ IComparableSample/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IComparableSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = "data1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string>? list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        string? line = sr.ReadLine();
+                        if (line != null) list.Add(line);
+                    }
+                    list.Sort();
+                    foreach (string item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
