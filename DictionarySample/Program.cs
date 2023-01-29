557a1d015f3805334eef0338877741c12c740d8b
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-29 00:30:31 -0300

    added dictionary sample

---
+++ DictionarySample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DictionarySample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "limonada";
+            cookies["email"] = "limonada@gmail.com";
+            cookies["phone"] = "911345678910";
+
+            foreach (KeyValuePair<string, string> item in cookies)
+                Console.WriteLine($"{item.Key}: {item.Value}");
+
+            if (cookies.Remove("email"))
+                Console.WriteLine("\nEmail key removed");
+
+            foreach (KeyValuePair<string, string> item in cookies)
+                Console.WriteLine($"{item.Key}: {item.Value}");
+
+            Console.ReadKey();
+        }
+    }
+}
