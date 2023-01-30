04b3b60bfea86ecab82a24911832090bc825ac79
Lucas Franco de Mello <luk4w@outlook.com>
2023-01-29 21:30:09 -0300

    another extension sample added

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,8 +6,12 @@ namespace ExtensionMethods
     {
         private static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2023, 01, 29, 21, 15, 0);
-            Console.WriteLine(dt.ElapsedTime());
+            // DateTime dt = new DateTime(2023, 01, 29, 21, 15, 0);
+            // Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
             Console.ReadKey();
         }
     }
